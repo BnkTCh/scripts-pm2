759a5fbbe51e80ad715c0d19ac0c478cb10d09a2
agentebtc <cryptoswam@gmail.com>
2021-04-30 15:19:33 -0500

    script para notificar estado de un proceso por sns

--- monitapps.sh
+++ monitapps.sh
@@ -4,33 +4,37 @@ proceso1=`pm2 status | sed '7q;d' | awk '{print $4,$18}' | grep -c online` #fiat
 proceso2=`pm2 status | sed '6q;d' | awk '{print $4,$18}' | grep -c online` #cleanup
 proceso3=`pm2 status | sed '5q;d' | awk '{print $4,$18}' | grep -c online` #checkbtcprice
 
-
 if [[ $proceso1 != 1 ]]
 then
-  echo "vamo a reiniciar esta vaina fiatapi"
-       
-#else
- #       echo "no pasa nda"
+  #echo "El proceso FIATAPI se encuentra detenido, por favor revisar"
+	var1="FIATAPI"       
+else
+        var=x
 fi
 
 
 if [[ $proceso2 != 1 ]]
 then
-  echo "vamo a reiniciar esta vaina cleanup"
-
-#else
- #       echo "no pasa nda"
+  #echo "El proceso CLEANUP se encuentra detenido, por favor revisar"
+	var2="CLEANUP"
+else
+        var=x
 fi
 
 
 if [[ $proceso3 != 1 ]]
 then
-  echo "vamo a reiniciar esta vaina checkbtcprice"
-
-#else
- #       echo "no pasa nda"
+  #echo "El proceso CHECKBTCPRICE se encuentra detenido, por favor revisar"
+	var3="CHECKBTCPRICE"
+else
+        var=x
 fi
 
 
+if [[ $var != x ]]
+then
+	echo "los servicios $var1 $var2 $var3 estan detenidos... aiuda!"
+fi
+echo $var
 
 exit
---
+++ monitapps2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+proceso1=`pm2 status | sed '7q;d' | awk '{print $4,$18}' | grep -c online` #fiatapi
+proceso2=`pm2 status | sed '6q;d' | awk '{print $4,$18}' | grep -c online` #cleanup
+proceso3=`pm2 status | sed '5q;d' | awk '{print $4,$18}' | grep -c online` #checkbtcprice
+
+if [[ $proceso1 = 1 ]]
+then
+        varOn1="FIATAPI"
+else
+	varOff1="FIATAPI"		        	
+fi
+
+
+if [[ $proceso2 = 1 ]]
+then
+        varOn2="CLEANUP"
+else
+	varOff2="CLEANUP"	
+fi
+
+
+if [[ $proceso3 = 1 ]]
+then
+        varOn3="CHECKBTCPRICE"	        
+else
+	varOff3="CHECKBTCPRICE"	
+fi
+
+varOn="$varOn1 $varOn2 $varOn3"
+var=`echo $varOn | grep -c "$varOn"`
+varOff="$varOff1 $varOff2 $varOff3"
+
+
+if [[ $var != 1 ]]
+then
+	#echo "se fue alv revisar $varOff"
+	curl -H "Content-Type: application/json" -X POST   -d '{"sms":"Hay procesos que no se están ejecutando, por favor revisarlos"}' https://4fj5ga6su4.execute-api.us-east-1.amazonaws.com/default/bnkmonitapps
+#else
+	
+fi 
+
+
+exit
+
